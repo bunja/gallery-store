eea5fddd9aca41693a7281f2e896ceb7e537bb9d
Larisa Kosareva <kos.lv@yandex.com>
2022-04-01 23:32:42 +0200

    images added to frontend, backend started

--- frontend/src/paintings.js
+++ frontend/src/paintings.js
@@ -2,7 +2,7 @@ const paintings = [
   {
     '_id': '1',
     'name': 'Playng Light Empire',
-    'image': '/images/lightEmpire.jpg',
+    'image': '/images/playing_light.jpg',
     'description':
       'Hommage On Empire of Light',
     'materials': 'Acryl on Canvas stretcher bar',
@@ -17,7 +17,7 @@ const paintings = [
   {
     '_id': '2',
     'name': 'Last sunny day',
-    'image': '/images/sunnyDay.jpg',
+    'image': '/images/sunny_day.jpg',
     'description':
       'Autumn sunny day',
     'materials': 'Acryl on Canvas stretcher bar',
@@ -33,7 +33,7 @@ const paintings = [
   {
     '_id': '3',
     'name': 'Baltic See',
-    'image': '/images/balticSee.jpg',
+    'image': '/images/baltic_sea.jpg',
     'description':
       'I painted this painting to commemorate the day my father was barried',
     'materials': 'Acryl on Canvas stretcher bar',
@@ -47,8 +47,8 @@ const paintings = [
   },
   {
     '_id': '4',
-    'name': 'Old house',
-    'image': '/images/oldHouse.jpg',
+    'name': 'Dots',
+    'image': '/images/dots.jpg',
     'description':
       'The house I was born in',
     'materials': 'Acryl on Canvas stretcher bar',
@@ -62,8 +62,8 @@ const paintings = [
   },
   {
     '_id': '5',
-    'name': 'Road',
-    'image': '/images/road.jpg',
+    'name': 'Night',
+    'image': '/images/night.jpg',
     'description':
       'Road',
     'materials': 'Acryl on Canvas stretcher bar',
@@ -71,6 +71,21 @@ const paintings = [
     'widht': '60 cm',
     'year': '2021',
     'price': 89.99,
+    'available': false,
+    'rating': 0.5,
+    'numReviews': 1,
+  },
+  {
+    '_id': '6',
+    'name': 'Light',
+    'image': '/images/light.jpg',
+    'description':
+      'Road',
+    'materials': 'Acryl on Canvas stretcher bar',
+    'height': '50 cm',
+    'widht': '50 cm',
+    'year': '2021',
+    'price': 69.99,
     'available': false,
     'rating': 0.5,
     'numReviews': 1,
