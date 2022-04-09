be886fda21b190c06f31dabe23daaf2179a7b119
Larisa Kosareva <kos.lv@yandex.com>
2022-04-09 22:00:40 +0200

    simplejwt, settings added, urlpatterns for tokenobtain

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView
+)
+
 urlpatterns = [
+    path('users/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    
     path('', views.getRoutes, name='routes'),
     path('paintings/', views.getPaintings, name='paintings'),
     path('paintings/<str:pk>', views.getPainting, name='painting'),
