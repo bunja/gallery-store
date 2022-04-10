d8d1ce78c7650fe986be354cf9477febb389c1ee
Larisa Kosareva <kos.lv@yandex.com>
2022-04-10 22:10:50 +0200

    routes protection added

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
+from django.contrib.auth.models import User
 from .models import Painting
 # from .paintings import paintings 
 from .serializers import PaintingSerializer, UserSerializer, UserSerializerWithToken
@@ -30,11 +32,21 @@ def getRoutes(request):
     return Response('Not welcomed you are')
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     
     serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+
 
 @api_view(['GET'])
 def getPaintings(request):
